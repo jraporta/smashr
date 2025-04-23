90b4658b3807f3d6f88ee9c75cb03e6fe412b32a
jraporta <jraporta@gmail.com>
2025-04-23 13:50:34 +0200

    first commit

---
+++ smashr-backend/table-manager/src/test/java/com/jraporta/table_manager/TableManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jraporta.table_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TableManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
