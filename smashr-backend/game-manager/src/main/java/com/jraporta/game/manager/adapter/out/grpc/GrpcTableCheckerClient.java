b95b740a53fd7f817983d2adad6cc188e5fab258
jraporta <jraporta@gmail.com>
2025-07-04 18:34:30 +0200

    (game-manager) implement TableQueryService in gRPC client

--- smashr-backend/game-manager/src/main/java/com/jraporta/game/manager/adapter/out/grpc/GrpcTableCheckerClient.java
+++ smashr-backend/game-manager/src/main/java/com/jraporta/game/manager/adapter/out/grpc/GrpcTableCheckerClient.java
@@ -1,15 +1,18 @@
 package com.jraporta.game.manager.adapter.out.grpc;
 
+import com.jraporta.game.manager.application.port.out.TableQueryService;
+import com.jraporta.game.manager.application.model.TableDetails;
 import com.jraporta.game.manager.domain.port.out.TableCheckerPort;
 import com.jraporta.technical.api.proto.TableCheckReply;
 import com.jraporta.technical.api.proto.TableCheckRequest;
+import com.jraporta.technical.api.proto.TableDetailsReply;
 import com.jraporta.technical.api.proto.TableServiceGrpc;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @AllArgsConstructor
 @Service
-public class GrpcTableCheckerClient implements TableCheckerPort {
+public class GrpcTableCheckerClient implements TableCheckerPort, TableQueryService {
 
     private final TableServiceGrpc.TableServiceBlockingStub tableServiceStub;
 
@@ -18,5 +21,12 @@ public class GrpcTableCheckerClient implements TableCheckerPort {
         TableCheckRequest req = TableCheckRequest.newBuilder().setTableId(tableId).build();
         TableCheckReply reply = tableServiceStub.checkTableExists(req);
         return reply.getExists();
+    }
+
+    @Override
+    public TableDetails getTableDetails(String tableId) {
+        TableCheckRequest req = TableCheckRequest.newBuilder().setTableId(tableId).build();
+        TableDetailsReply reply = tableServiceStub.getTableDetails(req);
+        return new TableDetails(tableId, reply.getName(), reply.getDescription());
     }
 }
