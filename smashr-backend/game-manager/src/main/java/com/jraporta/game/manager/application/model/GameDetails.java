c7b0463f71158ff2a3546d3cf1655a759923e32a
jraporta <jraporta@gmail.com>
2025-07-07 11:09:52 +0200

    refactor(game-manager) convert GameDetails and TableDetails to Java record

--- smashr-backend/game-manager/src/main/java/com/jraporta/game/manager/application/model/GameDetails.java
+++ smashr-backend/game-manager/src/main/java/com/jraporta/game/manager/application/model/GameDetails.java
@@ -1,24 +1,23 @@
 package com.jraporta.game.manager.application.model;
 
 import com.jraporta.game.manager.domain.model.game.*;
-import lombok.Getter;
 
-@Getter
-public class GameDetails {
-
-    private final String id;
-    private final TableDetails table;
-    private final GamePlayers players;
-    private final GameSchedule schedule;
-    private final GameStatus status;
-    private final GameResult result;
-
+public record GameDetails(
+        String id,
+        TableDetails table,
+        GamePlayers players,
+        GameSchedule schedule,
+        GameStatus status,
+        GameResult result
+) {
     public GameDetails(Game game, TableDetails tableDetails) {
-        this.id = game.getId();
-        this.table = tableDetails;
-        this.players = game.getPlayers();
-        this.schedule = game.getSchedule();
-        this.status = game.getStatus();
-        this.result = game.getResult();
+        this(
+                game.getId(),
+                tableDetails,
+                game.getPlayers(),
+                game.getSchedule(),
+                game.getStatus(),
+                game.getResult()
+        );
     }
 }
--- smashr-backend/game-manager/src/main/java/com/jraporta/game/manager/application/model/TableDetails.java
+++ smashr-backend/game-manager/src/main/java/com/jraporta/game/manager/application/model/TableDetails.java
@@ -1,14 +1,7 @@
 package com.jraporta.game.manager.application.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@Getter
-@AllArgsConstructor
-public class TableDetails {
-
-    private final String id;
-    private final String name;
-    private final String description;
-
-}
+public record TableDetails (
+        String id,
+        String name,
+        String description
+) {}
